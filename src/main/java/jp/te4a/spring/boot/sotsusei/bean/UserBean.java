68d1b5f6d3d1497f00e77a405d627f2a31735a97
Ryosuke0515 <te190762@gmail.com>
2022-11-17 09:44:34 +0900

    add: ユーザーテーブルのエンティティクラス作成

--- src/main/java/jp/te4a/spring/boot/sotsusei/bean/UserBean.java
+++ src/main/java/jp/te4a/spring/boot/sotsusei/bean/UserBean.java
@@ -1,9 +1,10 @@
 package jp.te4a.spring.boot.sotsusei.bean;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,9 +17,20 @@ import lombok.NoArgsConstructor;
 public class UserBean {
 
     @Id
-    private String username;
-
-    @JsonIgnore
+    @GeneratedValue
+    private Integer user_id;
+    @Column(nullable = false)
+    private String user_name;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
+    private String mail_address;
+    private String note;
+    @Column(nullable = false)
+    private boolean is_banned;
+    @Column(nullable = false)
+    private boolean is_admin;
+
+    
 
 }
