297202984b411c3ed42c7a4b71dc7ea03421b8c6
koji1325600 <te190088@.com>
2022-11-17 11:11:56 +0900

    add: リポジトリクラス作成

---
+++ src/main/java/jp/te4a/spring/boot/sotsusei/repository/GameRepository.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.sotsusei.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import jp.te4a.spring.boot.sotsusei.bean.GameBean;
+
+public interface GameRepository extends JpaRepository<GameBean, Integer>{
+	@Query("SELECT X FROM GameBean X ORDER BY X.game_id")
+	  List<GameBean> findAllOrderByGame_id();
+	  
+}
