a8d966ade6ce4fba15be9aa8329f9ab139af21d5
koji1325600 <te190088@.com>
2022-11-25 12:29:21 +0900

    update: ログイン機能追加

--- src/main/java/jp/te4a/spring/boot/sotsusei/security/LoginUserDetails.java
+++ src/main/java/jp/te4a/spring/boot/sotsusei/security/LoginUserDetails.java
@@ -8,7 +8,7 @@ import lombok.Data;
 public class LoginUserDetails extends User {
     private final UserBean user;
     public LoginUserDetails(UserBean userBean) {
-        super(userBean.getUser_name(), userBean.getPassword(),
+        super(userBean.getMail_address(), userBean.getPassword(),
         		AuthorityUtils.createAuthorityList("ROLE_USER"));
         this.user = userBean;
     }
