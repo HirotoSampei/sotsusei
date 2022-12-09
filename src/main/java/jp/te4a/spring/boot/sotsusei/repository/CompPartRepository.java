d5baabd7b48e3ed97561bc1c54d5966844fc6304
Ryosuke0515 <te190762@gmail.com>
2022-12-09 14:37:09 +0900

    add:参加者管理テーブルの操作

---
+++ src/main/java/jp/te4a/spring/boot/sotsusei/repository/CompPartRepository.java
@@ -0,0 +1,25 @@
+package jp.te4a.spring.boot.sotsusei.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import jp.te4a.spring.boot.sotsusei.bean.CompPartBean;
+import jp.te4a.spring.boot.sotsusei.bean.CompPartPrimaryKey;
+
+public interface CompPartRepository extends JpaRepository<CompPartBean, CompPartPrimaryKey>{
+
+    @Query("SELECT X FROM CompPartBean X WHERE X.comp_id = ?1")
+	List<CompPartBean> findByComp_id(Integer comp_id);
+
+    @Query("SELECT X.user_id FROM CompPartBean X WHERE X.comp_id = ?1")
+	List<Integer> findByUser_id(Integer comp_id);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM CompPartBean X WHERE X.user_id = ?1")
+    void deleteByuser_id(Integer user_id);
+}
