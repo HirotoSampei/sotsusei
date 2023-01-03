87b98e8538387b484d70e48e59dfd7f529663cb9
{koji1325600} <{te190088@gmail.com}>
2023-01-03 20:57:37 +0900

    update: エラーメッセージ追加

--- src/main/java/jp/te4a/spring/boot/sotsusei/form/UserForm.java
+++ src/main/java/jp/te4a/spring/boot/sotsusei/form/UserForm.java
@@ -13,20 +13,20 @@ import lombok.NoArgsConstructor;
 public class UserForm {
     private Integer user_id;
 
-    @NotBlank
-    @Length(max = 50)
+    @NotBlank(message = "名前を入力してください")
+    @Length(max = 50, message = "名前は50桁以内で入力してください")
     private String user_name;
 
-    @NotBlank
-    @Length(max = 100)
+    @NotBlank(message = "パスワードを入力してください")
+    @Length(max = 100, message = "パスワードは100桁以内で入力してください")
     private String password;
 
-    @NotBlank
+    @NotBlank(message = "メールアドレスを入力してください")
     @Email
-    @Length(max = 50)
+    @Length(max = 50, message = "メールアドレスは50桁以内で入力してください")
     private String mail_address;
 
-    @Length(max = 2000)
+    @Length(max = 2000, message = "自由記入欄は2000桁以内で入力してください")
     private String note;
 
     private boolean is_banned;
