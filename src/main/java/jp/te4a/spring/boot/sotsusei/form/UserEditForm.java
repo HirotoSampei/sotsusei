a6fdba3211f263cc9979c4f7844ad2348a887324
{koji1325600} <{te190088@gmail.com}>
2023-01-03 20:57:03 +0900

    add: ユーザー編集フォーム追加

---
+++ src/main/java/jp/te4a/spring/boot/sotsusei/form/UserEditForm.java
@@ -0,0 +1,29 @@
+package jp.te4a.spring.boot.sotsusei.form;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserEditForm {
+    private Integer user_id;
+
+    @NotBlank(message = "名前を入力してください")
+    @Length(max = 50, message = "名前は50桁以内で入力してください")
+    private String user_name;
+
+    private String password;
+
+    private String mail_address;
+
+    @Length(max = 2000, message = "自由記入欄は2000桁以内で入力してください")
+    private String note;
+
+    private boolean is_banned;
+    private boolean is_admin;
+}
