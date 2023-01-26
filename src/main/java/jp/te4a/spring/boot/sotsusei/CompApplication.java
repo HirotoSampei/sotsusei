34566c7dc226555342e579fa1d89cfd7018b219a
koji1325600 <te190088@.com>
2023-01-26 10:39:30 +0900

    fix: バグ修正

--- src/main/java/jp/te4a/spring/boot/sotsusei/CompApplication.java
+++ src/main/java/jp/te4a/spring/boot/sotsusei/CompApplication.java
@@ -1,9 +1,9 @@
 package jp.te4a.spring.boot.sotsusei;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-@SpringBootApplication
 public class CompApplication {
   public static void main(String[] args) {
     SpringApplication.run(CompApplication.class, args);
