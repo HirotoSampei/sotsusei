429f815ba4b46a700118d80ae4cfa384d3bf2dec
Ryosuke0515 <104479366+Ryosuke0515@users.noreply.github.com>
2023-01-26 10:34:41 +0900

    Update CompApplication.java

--- src/main/java/jp/te4a/spring/boot/sotsusei/CompApplication.java
+++ src/main/java/jp/te4a/spring/boot/sotsusei/CompApplication.java
@@ -2,6 +2,7 @@ package jp.te4a.spring.boot.sotsusei;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @SpringBootApplication
 public class CompApplication {
   public static void main(String[] args) {
